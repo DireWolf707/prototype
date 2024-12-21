5960078634d69505f9f701858a5c045780e16d06
Rahul <rahulsood707@gmail.com>
2024-12-21 12:16:19 +0530

    updated create post logic

--- lib/actions/post.ts
+++ lib/actions/post.ts
@@ -3,7 +3,9 @@
 import db from '@/drizzle/client'
 import { postTable } from '@/drizzle/schema'
 import { postT } from '../types'
+import { postSchema } from '../zodSchemas'
 
-export const createPostAction = async (data: postT) => {
+export const createPostAction = async (_data: postT) => {
+    const data = postSchema.parse(_data)
     return await db.insert(postTable).values(data).returning()
 }
--- lib/hooks/post.ts
+++ lib/hooks/post.ts
@@ -1,11 +1,17 @@
 import { useMutation } from '@tanstack/react-query'
 import { createPostAction } from '@/lib/actions/post'
 import { toast } from 'sonner'
+import { useRouter } from 'next/navigation'
 
 export const useCreatePost = () => {
+    const router = useRouter()
+
     return useMutation({
         mutationFn: createPostAction,
-        onSuccess: () => toast('cool'),
-        onError: () => toast('not cool'),
+        onSuccess: () => {
+            toast.success('Post created successfully')
+            router.push('/')
+        },
+        onError: () => toast.error('Post not created'),
     })
 }

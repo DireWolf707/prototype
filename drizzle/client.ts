b52796b48732ecb22d2c1b9b5491b432c810e57f
Rahul <rahulsood707@gmail.com>
2024-12-21 17:18:12 +0530

    removed dotenv

--- drizzle/client.ts
+++ drizzle/client.ts
@@ -1,5 +1,9 @@
 import { drizzle } from 'drizzle-orm/neon-http'
+import * as schema from './schema'
 
-const db = drizzle(process.env.DATABASE_URL!)
+if (!process.env.DATABASE_URL)
+    throw new Error('DATABASE_URL must be a Neon postgres connection string')
+
+const db = drizzle(process.env.DATABASE_URL, { schema })
 
 export default db
--- drizzle/config.ts
+++ drizzle/config.ts
@@ -1,14 +1,17 @@
-import dotenv from 'dotenv'
+import { loadEnvConfig } from '@next/env'
 import { defineConfig } from 'drizzle-kit'
 import path from 'path'
 
-dotenv.config({ path: path.resolve(process.cwd(), '.env.development') })
+loadEnvConfig(path.resolve(process.cwd(), '.env.local'))
+
+if (!process.env.DATABASE_URL)
+    throw new Error('DATABASE_URL must be a Neon postgres connection string')
 
 export default defineConfig({
     out: './drizzle',
     schema: './drizzle/schema.ts',
     dialect: 'postgresql',
     dbCredentials: {
-        url: process.env.DATABASE_URL!,
+        url: process.env.DATABASE_URL,
     },
 })

1a08efbb3db853580488a8b473b6f36a706fa52d
Rahul <rahulsood707@gmail.com>
2024-12-21 13:06:29 +0530

    updated db schema

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,9 +1,10 @@
 import { topics } from '@/lib/constants'
-import { pgTable, uuid, varchar } from 'drizzle-orm/pg-core'
+import { pgTable, uuid, varchar, timestamp } from 'drizzle-orm/pg-core'
 
 export const postTable = pgTable('post', {
-    id: uuid().defaultRandom().primaryKey(),
+    id: uuid().primaryKey().defaultRandom(),
     title: varchar({ length: 50 }).notNull(),
     content: varchar({ length: 50 }).notNull(),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
     topic: varchar({ length: 20, enum: topics }).notNull(),
 })

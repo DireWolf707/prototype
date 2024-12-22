c56e5fb1072f69a827ace36c669e28719535b50a
Rahul <rahulsood707@gmail.com>
2024-12-22 21:44:42 +0530

    added text break

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -15,14 +15,14 @@ export default function Home() {
                     key={post.id}
                     className="space-y-4 rounded-md border-2 p-4"
                 >
-                    <div className="border-b-4 text-lg font-extrabold">
+                    <p className="text-wrap break-words border-b-4 text-lg font-extrabold">
                         {post.title}
-                    </div>
+                    </p>
                     <div className="flex items-center justify-between">
                         <div className="text-sm font-extralight">
                             [{post.createdAt.toLocaleDateString()}]
                         </div>
-                        <span className="flex items-center justify-center rounded-full border-2 bg-red-800 px-2 font-extrabold text-neutral-100">
+                        <span className="flex items-center justify-center text-wrap break-words rounded-full border-2 bg-red-800 px-2 font-extrabold text-neutral-100">
                             {post.topic}
                         </span>
                     </div>
